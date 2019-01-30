2572cc54897db28aac11dc83b2c5b7e9f0df1ef9
Liza <liza123.97@mail.ru>
2019-01-30 23:38:00 +0200

    new version 2.0

---
+++ js/component.js
@@ -0,0 +1,12 @@
+class Component{
+    constructor({element}){
+        this._element = element,
+    }
+    show(){
+        this._element.hidden = false;
+
+    }
+    hide(){
+        this._element.hidden = true;
+    }
+}
